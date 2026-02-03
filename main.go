da098b3f28ef0458328b2049a171a23cd1c893e6
hexbabe <sean.xiaochen.yu@gmail.com>
2026-02-03 16:33:51 -0500

    Initial

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+	"runtime"
+
+	"github.com/viam-labs/modular-webcam/models"
+
+	mdcam "github.com/pion/mediadevices/pkg/driver/camera"
+	"go.viam.com/rdk/components/camera"
+	"go.viam.com/rdk/logging"
+	"go.viam.com/rdk/module"
+	"go.viam.com/rdk/resource"
+)
+
+var logger = logging.NewDebugLogger("modular-webcam")
+
+func main() {
+	if runtime.GOOS == "darwin" {
+		if err := mdcam.StartObserver(); err != nil {
+			logger.Errorw("failed to start camera observer", "error", err)
+			os.Exit(1)
+		}
+		defer mdcam.DestroyObserver()
+	}
+
+	module.ModularMain(
+		resource.APIModel{API: camera.API, Model: models.Webcam},
+	)
+}
